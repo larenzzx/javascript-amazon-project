d770eb5d2fd902e56a6a1aa61e518b816d38dbd3
larenzzx <larenztabotabo@gmail.com>
2025-09-28 21:35:08 +0800

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,17 +3,12 @@
 // 2. Generate the HTML
 // 3. Make it interactive
 
-async function getProducts() {
-  const res = await fetch("../backend/products.json");
-  const products = await res.json();
-  let productsGrid = document.querySelector(".js-products-grid");
-  productsGrid.innerHTML = "";
+let productsHTML = "";
 
-  products.forEach((product) => {
-    const productContainer = document.createElement("div");
-    productContainer.classList.add("product-container");
-    productContainer.innerHTML = `
-  <div class="product-image-container">
+products.forEach((product) => {
+  productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
           </div>
@@ -31,7 +26,7 @@ async function getProducts() {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${product.priceCents / 100}
           </div>
 
           <div class="product-quantity-container">
@@ -56,53 +51,49 @@ async function getProducts() {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
+          }">
             Add to Cart
           </button>
-  `;
-    productsGrid.appendChild(productContainer);
+        </div>
+    `;
+});
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
   });
-}
-getProducts();
+});
 
-// this is the first code
-// const products = [
-//   {
-//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//     rating: {
-//       stars: 4.5,
-//       count: 87,
-//     },
-//     priceCents: 1090,
-//   },
-//   {
-//     image: "images/products/intermediate-composite-basketball.jpg",
-//     name: "Intermediate Size Basketball",
-//     rating: {
-//       stars: 4,
-//       count: 127,
-//     },
-//     priceCents: 2095,
-//   },
-//   {
-//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-//     name: " Adults Plain Cotton T-Shirt - 2 Pack",
-//     rating: {
-//       stars: 4.5,
-//       count: 56,
-//     },
-//     priceCents: 799,
-//   },
-// ];
+// other method
+// async function getProducts() {
+//   const res = await fetch("../backend/products.json");
+//   const products = await res.json();
+//   let productsGrid = document.querySelector(".js-products-grid");
+//   productsGrid.innerHTML = "";
 
-// let productsHTML = "";
-
-// console.log(product);
-// products.forEach((product) => {
-//   productsHTML += `
-//         <div class="product-container">
-//           <div class="product-image-container">
+//   products.forEach((product) => {
+//     const productContainer = document.createElement("div");
+//     productContainer.classList.add("product-container");
+//     productContainer.innerHTML = `
+//   <div class="product-image-container">
 //             <img class="product-image"
 //               src="${product.image}">
 //           </div>
@@ -120,7 +111,7 @@ getProducts();
 //           </div>
 
 //           <div class="product-price">
-//             $${product.priceCents / 100}
+//             $${(product.priceCents / 100).toFixed(2)}
 //           </div>
 
 //           <div class="product-quantity-container">
@@ -148,11 +139,8 @@ getProducts();
 //           <button class="add-to-cart-button button-primary">
 //             Add to Cart
 //           </button>
-//         </div>
-//     `;
-// });
-
-// console.log(productsHTML);
-// document.querySelector(".js-products-grid").innerHTML = productsHTML;
-
-// other method
+//   `;
+//     productsGrid.appendChild(productContainer);
+//   });
+// }
+// getProducts();
