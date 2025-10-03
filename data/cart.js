824609cc927f27fda3b9217f0b861805c2f0a678
larenzzx <larenztabotabo@gmail.com>
2025-10-03 18:09:59 +0800

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,20 @@
 const removeTimeout = {};
 
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+// cart = [
+//   {
+//     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     quantity: 2,
+//   },
+//   {
+//     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+//     quantity: 1,
+//   },
+// ];
 
 export function addToCart(productId) {
   const selectElement = document.querySelector(
@@ -38,6 +43,8 @@ export function addToCart(productId) {
   removeTimeout[productId] = setTimeout(() => {
     added.classList.remove("added");
   }, 2000);
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -51,4 +58,6 @@ export function removeFromCart(productId) {
 
   cart = newCart;
   console.log(newCart);
+
+  saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -64,6 +64,12 @@ products.forEach((product) => {
     `;
 });
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+if (cart.length === 0) {
+  document.querySelector(".js-cart-quantity").innerHTML = 0;
+} else {
+  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+  document.querySelector(".js-cart-quantity").innerHTML = totalQuantity;
+}
 
 function updateCartQuantity() {
   let cartQuantity = 0;
@@ -83,66 +89,3 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     console.log(cart);
   });
 });
-
-// other method
-// async function getProducts() {
-//   const res = await fetch("../backend/products.json");
-//   const products = await res.json();
-//   let productsGrid = document.querySelector(".js-products-grid");
-//   productsGrid.innerHTML = "";
-
-//   products.forEach((product) => {
-//     const productContainer = document.createElement("div");
-//     productContainer.classList.add("product-container");
-//     productContainer.innerHTML = `
-//   <div class="product-image-container">
-//             <img class="product-image"
-//               src="${product.image}">
-//           </div>
-
-//           <div class="product-name limit-text-to-2-lines">
-//             ${product.name}
-//           </div>
-
-//           <div class="product-rating-container">
-//             <img class="product-rating-stars"
-//               src="images/ratings/rating-${product.rating.stars * 10}.png">
-//             <div class="product-rating-count link-primary">
-//              ${product.rating.count}
-//             </div>
-//           </div>
-
-//           <div class="product-price">
-//             $${(product.priceCents / 100).toFixed(2)}
-//           </div>
-
-//           <div class="product-quantity-container">
-//             <select>
-//               <option selected value="1">1</option>
-//               <option value="2">2</option>
-//               <option value="3">3</option>
-//               <option value="4">4</option>
-//               <option value="5">5</option>
-//               <option value="6">6</option>
-//               <option value="7">7</option>
-//               <option value="8">8</option>
-//               <option value="9">9</option>
-//               <option value="10">10</option>
-//             </select>
-//           </div>
-
-//           <div class="product-spacer"></div>
-
-//           <div class="added-to-cart">
-//             <img src="images/icons/checkmark.png">
-//             Added
-//           </div>
-
-//           <button class="add-to-cart-button button-primary">
-//             Add to Cart
-//           </button>
-//   `;
-//     productsGrid.appendChild(productContainer);
-//   });
-// }
-// getProducts();
